7dfd1ac02490f53e6590030d632b476bf98ebbe1
ismailmin <mingirismail@gmail.com>
2024-01-03 20:18:46 +0000

    added explanation

--- test-local-deployment.sh
+++ test-local-deployment.sh
@@ -1,1 +1,5 @@
+##########################
+# bu script dev-server icin uygulamayi mysql db kullanarak Docker ile ayaga kaldirir
+##########################
+
 docker-compose -f docker-compose-local.yml up
