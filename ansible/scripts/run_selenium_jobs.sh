6556db45b3d413ad038d684cbc546b880909afd7
ismailmin <mingirismail@gmail.com>
2024-01-07 11:47:29 +0000

    added qa automation pipeline for dev

---
+++ ansible/scripts/run_selenium_jobs.sh
@@ -0,0 +1,3 @@
+PATH="$PATH:/usr/local/bin"
+ansible-playbook -vvv --connection=local --inventory 127.0.0.1, --extra-vars "workspace=${WORKSPACE} master_public_ip=${MASTER_PUBLIC_IP}" ./ansible/playbooks/pb_run_selenium_jobs.yaml
+# kayit icin, calistirilmayacak file
